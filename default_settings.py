43c9d336d1c9a9cc187b667c5cd92de3eb220fa9
Daniel Lawrence <dannyla@linux.com>
2016-12-13 20:27:25 +1100

    default_settings

---
+++ default_settings.py
@@ -0,0 +1,17 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEMO_RECORDS = [
+    ('mail', 'https://gmail.com'),
+    ('git', 'https://gitlab.com{/search?terms=^}')
+]
+
+ADD_DEMO_RECORDS = True
+
+TITLE = 'Go Links'
+SUB_TITLE = 'Short link service for internal use'
+
+PORT = 5000
+HOSTNAME = 'localhost'
+
+DEBUG = True

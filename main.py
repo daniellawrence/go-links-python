81096213d813e081b0cd8932a1cba66229d2711f
Daniel Lawrence <dannyla@linux.com>
2016-12-13 20:08:05 +1100

    Added Database and Search /golinks/search/<search_text>
    If you visit a go-link that is missing, you will be redirect to search results.
    Create and Updates of go-links are pushed into the database for safe keeping.
    
    Database and other settings now live in `settings.py`

--- main.py
+++ main.py
@@ -2,72 +2,136 @@
 import re
 
 from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import or_
+import settings
 
 app = Flask(__name__)
 
-go_link_database = {}
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS or False
+db = SQLAlchemy(app)
 
 
-class GoRecord(object):
+class GoRecord(db.Model):
+    __tablename__ = 'gorecord'
+    name = db.Column(db.String(50), primary_key=True, unique=True)
+    url = db.Column(db.String(255))
+    owner = db.Column(db.String(50))
 
-    def __init__(self, name, url):
+    def __init__(self, name, url, owner=None):
         self.name = name
         self.url = url
-        self.url_plain = re.sub('{.*}', '', url)
-        self.url_optional = None
-        url_optional = re.match('.*{(.*)}.*', url)
-        if url_optional:
-            self.url_optional = url_optional.groups()[0]
+        self.owner = owner
 
     def __repr__(self):
         return "<GoRecord {.name}>".format(self)
 
     def link(self, optional=None):
-        if optional and self.url_optional:
-            optional_args = self.url_optional.replace('^', optional)
-            return '{}{}'.format(self.url_plain, optional_args)
-        return self.url_plain
+        url_plain = re.sub('{.*}', '', self.url)
+        url_optional = ''
+
+        try:
+            url_optional = re.match('.*{(.*)}.*', self.url)
+            url_optional = url_optional.groups()[0]
+        except AttributeError:
+            url_optional = ''
+
+        if optional:
+            optional_args = url_optional.replace('^', optional)
+            return '{}{}'.format(url_plain, optional_args)
+
+        return url_plain
 
 
+def render_template_with_settings(template, **kwargs):
+    return render_template(template, settings=settings, **kwargs)
+
+    
 @app.route('/')
 def index():
-    return render_template(
+    return render_template_with_settings(
         'index.html',
-        go_link_database=go_link_database
+        all_records=GoRecord.query.all()
     )
 
 
 @app.route('/<name>')
 @app.route('/<name> <optional_argument>')
 def redirect_to_link(name, optional_argument=None):
-    record = go_link_database.get(name)
+    record = GoRecord.query.filter_by(name=name).first()
+
+    if record is None:
+        golink_search_url = "{}/{}".format(url_for('golink_search'), name)
+        return redirect(golink_search_url)
+
     link = record.link(optional_argument)
     return redirect(link)
 
 
 @app.route('/golinks/edit/<name>/', methods=['GET'])
 def golink_edit(name):
-    go_record = go_link_database.get(name)
-    return render_template(
+    record = GoRecord.query.filter_by(name=name).first_or_404()
+
+    return render_template_with_settings(
         'index.html',
-        edit_record=go_record,
-        go_link_database=go_link_database
+        edit_record=record,
+        all_records=GoRecord.query.all()
     )
 
+
+@app.route('/golinks/search', methods=['POST'])
+@app.route('/golinks/search/<search>', methods=['GET'])
+def golink_search(search=None):
+
+    if search is None:
+        search = request.form.get('search')
+        golink_search_url = "{}/{}".format(url_for('golink_search'), search)
+        return redirect(golink_search_url)
+
+    search_like = '%{}%'.format(search)
+    search_q = GoRecord.query.filter(or_(
+        GoRecord.name.like(search_like),
+        GoRecord.url.like(search_like),
+    ))
+
+    all_records = search_q.all()
+
+    return render_template_with_settings(
+        'index.html',
+        all_records=all_records,
+        previous_search_text=search
+    )
+    
 
 @app.route('/golinks/submit', methods=['POST'])
 def golink_submit():
     name = request.form.get('name')
     url = request.form.get('url')
-    go_record = GoRecord(name, url)
-    go_link_database[name] = go_record
+
+    record = GoRecord.query.filter_by(name=name).first()
+    if record:
+        record.url = url
+    else:
+        record = GoRecord(name, url)
+
+    db.session.add(record)
+    db.session.commit()
+
     return redirect(url_for('.index'))
 
 
 if __name__ == '__main__':
-    # DEMO GoRecords
-    go_link_database['mail'] = GoRecord('mail', 'https://gmail.com')
-    go_link_database['git'] = GoRecord('git', 'https://gitlab.com{/search?terms=^}')
+    db.create_all()
 
+    for name, url in settings.DEMO_RECORDS:
+        
+        record = GoRecord(name, url)
+        try:
+            db.session.add(record)
+            db.session.commit()
+        except Exception:
+            pass
+    
     app.debug = True
     app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,10 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEMO_RECORDS = [
+    ('mail', 'https://gmail.com'),
+    ('git', 'https://gitlab.com{/search?terms=^}')
+]
+
+TITLE = 'Go Links'
+SUB_TITLE = 'Short link service for internal use'
